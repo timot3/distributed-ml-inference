351a9549fb2abe18ed83d2b6d08c6ddfc9691590
Timothy Vitkin <timothy.vitkin@gmail.com>
2022-10-18 22:30:04 -0500

    Node joins work for up to 4 nodes

--- run_node.py
+++ run_node.py
@@ -19,8 +19,13 @@ if __name__ == "__main__":
     HOST, PORT = "127.0.0.1", get_any_open_port()
     INTRODUCER_HOST, INTRODUCER_PORT = "127.0.0.1", 8080
     # create a udp server that resuses the address
-    with NodeUDPServer(HOST, PORT, INTRODUCER_HOST, INTRODUCER_PORT) as server:
+    with NodeUDPServer(HOST, PORT, INTRODUCER_HOST, INTRODUCER_PORT, is_introducer=False) as server:
         server.allow_reuse_address = True
+        print("binding")
         server.server_bind()
+        print("activating")
         server.server_activate()
+        print("joining")
+        server.join_network()
+        print("serving")
         server.serve_forever()
