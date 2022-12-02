8fe78bb6e9576ebf755989a392e426bd2dd1334e
Timothy Vitkin <timothy.vitkin@gmail.com>
2022-12-02 17:31:43 -0600

    Make infer and infer_batch async

--- Node/LoadBalancer/LoadBalancer.py
+++ Node/LoadBalancer/LoadBalancer.py
@@ -3,15 +3,18 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from Node.node import NodeTCPServer
+    from Node.nodetypes import Member, MembershipList
 
 
 class LoadBalancer:
     """Load balancer for the ML queries"""
 
-    def __init__(self, node: "Node"):
+    def __init__(self, node: "NodeTCPServer"):
         self.node = node
         self.node.load_balancer = self
 
-    def get_best_node(self, query):
+    def get_best_node(self, query) -> "Member":
         """Returns the best node for the query"""
-        return self.node
+        # get the node with the least amount of active queries
+        # if there is a tie, choose the node with the least amount of total queries
+        pass
